0a5029a21f6220aeac3c8192a96db7ada351412f
Sebastian Poliak <sebastian.poliak@mercadolibre.com>
2018-12-31 04:42:32 -0300

    Add apartments view

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Button from './Button';
 import InputText from './InputText';
 
-const SearchBox = props => {
+const SearchBox = () => {
   return (
-    <form action="/location">
+    <form action="/apartments">
       <InputText name="location" type="text" placeholder="Search by location" />
       <Button primary>
         Search
---
+++ client/src/views/Apartments.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Apartments = props => {
+  const params = new URLSearchParams(window.location.search);
+
+  console.log(params);
+
+  return (
+    <div className="container">
+      
+    </div>
+  );
+}
+
+export default Apartments;
